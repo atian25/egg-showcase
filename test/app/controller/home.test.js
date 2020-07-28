b67242a0cc35334c6fe616c826260f816fc87eb4
TZ <atian25@qq.com>
2020-07-28 14:49:18 +0800

    test: for getFileStream limit

--- test/app/controller/home.test.js
+++ test/app/controller/home.test.js
@@ -17,4 +17,32 @@ describe('test/app/controller/home.test.js', () => {
       .expect('hi, egg')
       .expect(200);
   });
+
+  it.only('should upload with limit', async () => {
+    return app.httpRequest()
+      .post('/')
+      .attach('bigfile', __dirname + '/bigfile.txt', 'bigfile.txt')
+      .field('caption', 'My cats')
+      .expect('Request file too large, please check multipart config, fileSize: 1mb')
+      .expect(200);
+  });
+
+  it.only('should upload with limit 1024', async () => {
+    return app.httpRequest()
+      .post('/')
+      .query('limit=1024')
+      .attach('bigfile', __dirname + '/bigfile.txt', 'bigfile.txt')
+      .field('caption', 'My cats')
+      .expect('Request file too large, please check multipart config, fileSize: 1024')
+      .expect(200);
+  });
+
+  it.only('should upload with limit 1024 pass', async () => {
+    return app.httpRequest()
+      .post('/')
+      .query('limit=1024')
+      .attach('small', Buffer.from('abc'), 'small.txt')
+      .expect('small.txt')
+      .expect(200);
+  });
 });
